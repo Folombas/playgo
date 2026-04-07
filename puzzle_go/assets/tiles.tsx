5477531cdc5ce3d7c8cf4b7ff2a9d718d7bce7b7
Alex <folombas@yandex.ru>
2026-04-08 00:54:49 +0300

    Go99 - Revert ebitenui attempt, keep working sprite+Tiled UI

---
+++ puzzle_go/assets/tiles.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="game_tiles" tilewidth="64" tileheight="64" tilecount="6" columns="6">
+ <image source="tileset.png" width="384" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="border"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="inner"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="frame"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="glow"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="center"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="type" value="star"/>
+  </properties>
+ </tile>
+</tileset>
